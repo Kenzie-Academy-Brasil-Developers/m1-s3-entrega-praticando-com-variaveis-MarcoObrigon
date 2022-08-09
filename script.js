2cc6902303290387bab6abd2d4b7992397a1f555
Marco <obrigon19@outlook.com>
2022-08-09 12:51:55 -0300

    inserido variáveis

---
+++ script.js
@@ -0,0 +1,30 @@
+let nome ="Jhon"
+console.log (nome)
+
+let sobrenome ="Doyle"
+console.log (sobrenome)
+
+let anoDeNascimento = 1991
+let anoAtual = 2022
+
+let idade = (anoAtual - anoDeNascimento)
+console.log (idade)
+
+let cidade = "Curitiba"
+console.log (cidade)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
